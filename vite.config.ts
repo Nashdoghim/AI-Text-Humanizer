ff2c8ebf9812e5a711e95bd2e5c3f8f683ab0dd4
Nashdoghim <nashdoghim@gmail.com>
2025-08-13 19:13:26 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // This helps avoid routing issues
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
